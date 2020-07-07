6bae73188980ac04a52e8da9e1b15784ed939855
skdog <51351911+souravendra@users.noreply.github.com>
2020-07-08 03:11:21 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,87 @@
+const Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `$`
+    if (message.substring(0, 1) == '$') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+
+		    case 'hi':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Greetings, '+user+'!'
+                });
+                break;
+
+            case 'charas-ganja':
+                bot.sendMessage({
+                     to: channelID,
+                     message: 'Mereko Pyaara!'
+                 });
+                 break;
+
+            case 'bokachoda':
+                bot.sendMessage({
+                     to: channelID,
+                     message: 'Tor baap ke bol sala'
+                 });
+                 break;
+
+            case 'shuorer-bachha':
+                bot.sendMessage({
+                     to: channelID,
+                     message: 'chup bokchod'
+                 });
+                 break;
+            
+            case 'bagchi':
+                bot.sendMessage({
+                     to: channelID,
+                     message: 'moha maga'
+                 });
+                 break;
+            
+            case 'tor-maa':
+                bot.sendMessage({
+                     to: channelID,
+                     message: 'Mangsho kaate'
+                 });
+                 break;
+            
+            // case 'help':
+            //     bot.sendMessage({
+            //         to: channelID,
+            //         message: 'Available commands are'
+            //     })
+            // Just add any case commands if you want to..
+         }
+     }
+});
